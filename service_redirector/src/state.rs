6d1135f19daabb33394660750c01f76ac4895958
ishworii <ishworkhanal21@gmail.com>
2025-11-26 23:33:07 -0500

    added state in redirectory service

---
+++ service_redirector/src/state.rs
@@ -0,0 +1,31 @@
+use deadpool_postgres::{
+    Config as PgConfig, Manager, ManagerConfig, Pool as PgPool, RecyclingMethod,
+    Runtime as PgRuntime,
+};
+use deadpool_redis::Pool as RedisPool;
+use std::{str::FromStr, sync::Arc};
+use tokio_postgres::{Config, NoTls};
+
+pub struct AppState {
+    pub redis_pool: RedisPool,
+    pub db_read_pool: PgPool,
+}
+
+pub type SharedState = Arc<AppState>;
+
+pub fn create_pg_pool(db_url: &str) -> Result<PgPool, Box<dyn std::error::Error>> {
+    let pg_config = db_url.parse::<Config>()?;
+    let mgr_config = ManagerConfig {
+        recycling_method: RecyclingMethod::Fast,
+    };
+    let mgr = Manager::from_config(pg_config, NoTls, mgr_config);
+    let pool = PgPool::builder(mgr).max_size(16).build()?;
+
+    Ok(pool)
+}
+
+pub fn create_redis_pool(redis_url :&str) -> Result<RedisPool,Box<dyn std::error::Error>>{
+    let cfg = deadpool_redis::Config::from_url(redis_url);
+    let pool = cfg.create_pool(Some(deadpool_redis::Runtime::Tokio1))?;
+    Ok(pool)
+}
